a77458847330cdddf38361044b653d992daf6d7f
Maximiliano Bernard <bmaximiliano0210@gmail.com>
2024-03-20 23:26:22 -0300

    Add human codebreaker logic

--- mastermind.rb
+++ mastermind.rb
@@ -63,16 +63,34 @@ module Squares
   def blank_sq
     '□'
   end
+
+  def white_sq
+    '■'
+  end
+
+  def get_color(color)
+    colors = {
+      'red' => red_sq,
+      'green' => green_sq,
+      'yellow' => yellow_sq,
+      'blue' => blue_sq,
+      'pink' => pink_sq,
+      'lightblue' => light_blue_sq
+    }
+    colors[color]
+  end
 end
 
 # Board class
 class Board
+  MAX_TURNS = 12
+  CODE_LENGTH = 4
   include Squares
   attr_accessor :board, :title, :board_header
 
   def initialize
-    @board = Array.new(12) { Array.new(4, '□') }
-    @info = Array.new(12) { Array.new(4, '□') }
+    @board = Array.new(MAX_TURNS) { Array.new(CODE_LENGTH, '□') }
+    @info = Array.new(MAX_TURNS) { Array.new(CODE_LENGTH, '□') }
     @title = 'Mastermind'
     @board_header = '   Board' + 'Info  '.rjust(12)
   end
@@ -81,39 +99,43 @@ class Board
     puts @title.center(@board_header.length)
     puts @board_header
     @board.each_with_index do |row, idx|
-      puts row.join(' ').rjust(9) + (@info[idx].join(' ') + '  ').rjust(12)
+      puts ' ' + row.join(' ') + '  ' + (@info[idx].join(' ') + '  ')
     end
   end
 
   def place_peg(color, row, column)
-    case color
-    when 'red'
-      @board[row][column] = red_sq
-    when 'green'
-      @board[row][column] = green_sq
-    when 'yellow'
-      @board[row][column] = yellow_sq
-    when 'blue'
-      @board[row][column] = blue_sq
-    when 'pink'
-      @board[row][column] = pink_sq
-    when 'lightblue'
-      @board[row][column] = light_blue_sq
-    end
+    @board[row][column] = get_color(color)
   end
 
   def remove_peg(row, column)
     @board[row][column] = blank_sq
   end
+
+  def get_row(row)
+    @board[row]
+  end
+
+  def set_info(row, exact_matches, color_matches)
+    offset = 0
+    exact_matches.times do |i|
+      @info[row][i] = '■'.red
+      offset += 1
+    end
+    color_matches.times { |i| @info[row][i + offset] = white_sq }
+  end
 end
 
 # Mastermind class
 class Mastermind
+  include Squares
+  MAX_TURNS = 12
+  CODE_LENGTH = 4
   def initialize
     @board = Board.new
     @turn = 0 # Same as row
-    @code = generate_code
     @colors = %w[red green yellow blue pink lightblue]
+    @state = 'playing'
+    @code = generate_code
   end
 
   def print_instructions
@@ -128,7 +150,7 @@ class Mastermind
   end
 
   def parse_input
-    input = gets.chomp.downcase.split(' ').map(&:i)
+    input = gets.chomp.downcase
     if input == 'board'
       @board.print_board
     elsif input == 'instructions'
@@ -136,25 +158,65 @@ class Mastermind
     elsif input == 'confirm'
       confirm_guess
       return true
+    elsif input == 'code'
+      puts @code
     elsif input.include?('remove')
-      @board.remove_peg(@turn, input[1])
+      input = input.split(' ')
+      @board.remove_peg(@turn, input[1].to_i - 1)
     else
-      @board.place_peg(input[0], @turn, input[1])
+      input = input.split(' ')
+      @board.place_peg(input[0], @turn, input[1].to_i - 1) if @colors.include?(input[0])
     end
     false
   end
 
-  def confirm_guess; end
+  def confirm_guess
+    compare_guess(current_guess)
+  end
+
+  def compare_guess(guess)
+    exact_matches = 0
+    color_matches = 0
+    guess.each_with_index do |peg, idx|
+      exact_matches += peg == @code[idx] ? 1 : 0
+      color_matches -= peg == @code[idx] ? 1 : 0
+    end
+    guess.uniq.each { |peg| color_matches += @code.count(peg) }
+    @board.set_info(@turn, exact_matches, color_matches)
+    return @state = 'win' if exact_matches == CODE_LENGTH
+
+    @state = 'lose' if @turn == MAX_TURNS - 1
+  end
+
+  def generate_code
+    code = []
+    CODE_LENGTH.times { code << get_color(@colors.sample) }
+    code
+  end
+
+  def current_guess
+    @board.get_row(@turn)
+  end
 
   def play_as_codebreaker
-    loop do
-      @board.print_board
-      puts "Turn: #{@turn + 1}"
-      until parse_input
+    @board.print_board
+    until @turn == MAX_TURNS || @state != 'playing'
+      if parse_input
+        @board.print_board
+        @turn += 1
       end
-      @turn += 1
+    end
+    announce_winner
+  end
+
+  def announce_winner
+    if @state == 'win'
+      puts 'You win!'
+    else
+      puts 'You lose!'
     end
   end
 end
-board = Board.new
-board.print_instructions
+
+game = Mastermind.new
+game.play_as_codebreaker
